f97dae4a179cf95d5b40a441bd4d9c2e2a362e7b
isohata2011 <isohata2011@gmail.com>
2014-07-14 23:41:14 +0900

    カレンダー表示（HTMLサンプルコピー）

---
+++ src/main/java/jp/tokyo/mytiny/MyDataRepository.java
@@ -0,0 +1,12 @@
+package jp.tokyo.mytiny;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+@Repository
+public interface MyDataRepository
+        extends JpaRepository<MyData, Integer> {
+
+    public List<MyData> findByName(String name);
+}
---
+++ src/main/java/jp/tokyo/mytiny/SampleController.java
@@ -0,0 +1,51 @@
+package jp.tokyo.mytiny;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class SampleController {
+ 
+    @Autowired
+    MyDataRepository repository;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        //mv.setViewName("index");
+        Iterable<MyData> list = repository.findAll();
+        model.addAttribute("datas",list);
+        return "index";
+    }
+ 
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public String helo(Model model,
+            @RequestParam("name") String name,
+            @RequestParam("mail") String mail,
+            @RequestParam("tel") String tel,
+            @RequestParam("age") int age) {
+        MyData mydata = new MyData(name,mail,tel,age);
+        repository.saveAndFlush(mydata);
+        Iterable<MyData> list = repository.findAll();
+        model.addAttribute("datas",list);
+        return "index";
+    }
+
+    @RequestMapping(value="/find", method=RequestMethod.POST)
+	public String find(Model model, @RequestParam("find") String fstr) {
+        Iterable<MyData> list = repository.findByName(fstr);
+        model.addAttribute("datas", list);
+        return "index";
+    }
+    
+    
+    @RequestMapping("/schedule/list")
+    @ResponseBody
+    public String schedule(Model model) {
+        return "[]";
+    }    
+}
