09f07eb73cae93544805230857f3107cf13dcf58
isohata2011 <isohata2011@gmail.com>
2014-07-15 23:15:37 +0900

    add angularjs ui-calendar

---
+++ src/main/resources/static/js/calendarDemo.js
@@ -0,0 +1,113 @@
+/**
+ * calendarDemoApp - 0.1.3
+ */
+angular.module('calendarDemoApp', ['ui.calendar', 'ui.bootstrap']);
+
+function CalendarCtrl($scope) {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    
+    $scope.changeTo = 'Japanese';
+    /* event source that pulls from google.com */
+    $scope.eventSource = {
+    };
+    /* event source that contains custom events on the scope */
+    $scope.events = [
+      {title: '１日中イベント',start: new Date(y, m, 1)},
+      {title: '長いイベント',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+      {title: '誕生日',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+      {title: 'クリックするとグーグルへ飛ぶ',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+    ];
+    /* event source that calls a function on every view switch */
+    $scope.eventsF = function (start, end, callback) {
+      var s = new Date(start).getTime() / 1000;
+      var e = new Date(end).getTime() / 1000;
+      var m = new Date(start).getMonth();
+      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      callback(events);
+    };
+
+
+    /* alert on eventClick */
+    $scope.alertOnEventClick = function( event, allDay, jsEvent, view ){
+        $scope.alertMessage = (event.title + ' はクリックされました。 ');
+    };
+    /* alert on Drop */
+     $scope.alertOnDrop = function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view){
+       $scope.alertMessage = ('Event Droped to make dayDelta ' + dayDelta);
+    };
+    /* alert on Resize */
+    $scope.alertOnResize = function(event, dayDelta, minuteDelta, revertFunc, jsEvent, ui, view ){
+       $scope.alertMessage = ('Event Resized to make dayDelta ' + minuteDelta);
+    };
+    /* add and removes an event source of choice */
+    $scope.addRemoveEventSource = function(sources,source) {
+      var canAdd = 0;
+      angular.forEach(sources,function(value, key){
+        if(sources[key] === source){
+          sources.splice(key,1);
+          canAdd = 1;
+        }
+      });
+      if(canAdd === 0){
+        sources.push(source);
+      }
+    };
+    /* add custom event*/
+    $scope.addEvent = function() {
+      $scope.events.push({
+        title: 'Open Sesame',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        className: ['openSesame']
+      });
+    };
+    /* remove event */
+    $scope.remove = function(index) {
+      $scope.events.splice(index,1);
+    };
+    /* Change View */
+    $scope.changeView = function(view,calendar) {
+      calendar.fullCalendar('changeView',view);
+    };
+    /* Change View */
+    $scope.renderCalender = function(calendar) {
+      if(calendar){
+        calendar.fullCalendar('render');
+      }
+    };
+    /* config object */
+    $scope.uiConfig = {
+      calendar:{
+        height: 450,
+        editable: true,
+        header:{
+          left: 'title',
+          center: '',
+          right: 'today prev,next'
+        },
+        eventClick: $scope.alertOnEventClick,
+        eventDrop: $scope.alertOnDrop,
+        eventResize: $scope.alertOnResize
+      }
+    };
+
+    $scope.changeLang = function() {
+      if($scope.changeTo === 'Japanese'){
+        $scope.uiConfig.calendar.dayNames = ["日曜", "月曜", "火曜", "水曜", "木曜", "金曜", "土曜"];
+        $scope.uiConfig.calendar.dayNamesShort = ["日", "月", "火", "水", "木", "金", "土"];
+        $scope.changeTo= 'English';
+      } else {
+        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        $scope.changeTo = 'Japanese';
+      }
+    };
+    /* event sources array*/
+    $scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
+}
+/* EOF */
