6e7cd051b57f70a11e5c8dab2bcb6c24189cb595
isohata2011 <isohata2011@gmail.com>
2014-08-31 22:10:53 +0900

    add @EntityScan

--- src/main/java/jp/tokyo/mytiny/App.java
+++ src/main/java/jp/tokyo/mytiny/App.java
@@ -2,6 +2,7 @@ package jp.tokyo.mytiny;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("jp.tokyo.mytiny")
+@EntityScan("jp.tokyo.mytiny.domain")
 public class App {
  
     public static void main(String[] args) {
